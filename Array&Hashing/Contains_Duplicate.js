cc95e7b97ed8f874a9fde9d39f2ee327e4e576b3
Phinnf <bnm915915@gmail.com>
2026-06-08 17:43:19 +0700

    Array and String with hadDuplicate&isAnagram

---
+++ Array&Hashing/Contains_Duplicate.js
@@ -0,0 +1,25 @@
+export class ArraysAndHashing {
+  /**
+   * @param {number[]} nums
+   * @return {boolean}
+   */
+  hasDuplicate(nums) {
+    const uniqueArray = [...new Set(nums)];
+    return uniqueArray.length < nums.length;
+  }
+  /**
+   * @param {string} s
+   * @param {string} t
+   * @return {boolean}
+   */
+  isAnagram(s, t) {
+    if (s.length === t.length) {
+      const charSArray = [...s];
+      const chatTArray = [...t];
+      return charSArray.sort().join("") === chatTArray.sort().join("");
+    }
+  }
+}
+
+const TestFunction = new ArraysAndHashing();
+console.log(TestFunction.isAnagram("car", "rac"));
